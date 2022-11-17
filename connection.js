0ec6cd1c634713f8a8b3faf8bcdcd7327c343123
Harshpatel2910 <hppatel530@gmail.com>
2022-11-17 21:13:58 +0530

    test

--- connection.js
+++ connection.js
@@ -2,6 +2,7 @@ const {Client} = require('pg');
 
 
 const client = new Client({
+    
 
     host:'localhost',
     user:'postgres',
--- databasePg.js
+++ databasePg.js
@@ -2,12 +2,14 @@ const {Client} = require('pg');
 
 
 const client = new Client({
+    
 
     host:'localhost',
     user:'postgres',
     port:5432,
     password:'Harsh12@',
     database:'postgres'
+
     
 });
 
