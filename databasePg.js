b1d344015707a6f1e84ba34e01c253359f7ed2d8
Harshpatel2910 <hppatel530@gmail.com>
2022-11-17 19:22:50 +0530

    initial commit

---
+++ databasePg.js
@@ -0,0 +1,32 @@
+const {Client} = require('pg');
+
+
+const client = new Client({
+
+    host:'localhost',
+    user:'postgres',
+    port:5432,
+    password:'Harsh12@',
+    database:'postgres'
+});
+
+
+
+client.connect();
+
+
+client.query(`Select * from customer`,(res,err)=>{
+
+
+    if(!err)
+    {
+        console.log(res.rows);
+    }
+    else
+    {
+        console.log(err);
+    }
+    client.end;
+
+
+})
