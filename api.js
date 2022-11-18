34ff2129666c6431a5af035d04f493b16357c0d1
parth-1205 <parthprajapati97239@gmail.com>
2022-11-18 12:47:45 +0530

    Add HomePage's HTML and CSS Files

--- api.js
+++ api.js
@@ -1,49 +1,31 @@
-const client = require('./connection');
+const client = require("./connection");
 
-const express = require('express');
-
+const express = require("express");
 
 const app = express();
 
-
 app.listen(3000, () => {
-
-    console.log('Server is running on port 3000');
-})
-
-
-
+  console.log("Server is running on port 3000");
+});
 
 client.connect();
 
-app.get('/customer', (req, res) => {
+app.get("/customer", (req, res) => {
+  client.query(`set search_path to "groupId4_S10_G3"`, (err, result) => {
+    if (!err) {
+      console.log("Connected to database");
+    } else {
+      console.log(err);
+    }
+  });
 
+  client.query(`Select * from customer`, (err, result) => {
+    if (!err) {
+      res.send(result.rows);
+    } else {
+      console.log(err);
+    }
+  });
 
-    client.query(`set search_path to "groupId4_S10_G3"`,(err,result)=>{
-
-        if(!err)
-        {
-            console.log("Connected to database");
-        }
-        else{
-            console.log(err);
-        }
-        
-        
-    });
-
-    client.query(`Select * from customer`, (err, result) => {
-
-        if (!err) {
-            res.send(result.rows);
-        }
-        else {
-            console.log(err);
-        }
-    })
-
-    client.end;
-})
-
-
-
+  client.end;
+});
