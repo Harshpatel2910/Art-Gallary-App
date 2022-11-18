ff3d32181f1ee6450c843474bcc267f6e1693ce1
parth-1205 <parthprajapati97239@gmail.com>
2022-11-18 23:18:52 +0530

    Perfect

--- api.js
+++ api.js
@@ -1,50 +1,39 @@
-const client = require('./connection');
+const client = require("./connection");
 
-const express = require('express');
-
+const express = require("express");
 
 const app = express();
 
-
 app.listen(3000, () => {
-
-    console.log('Server is running on port 3000');
-})
-
-
-
+  console.log("Server is running on port 3000");
+});
 
 client.connect();
 
-app.get('/customer', (req, res) => {
+//register view engine
+app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
+  //res.sendFile("./Front-End/HomePage/index.html", { root: __dirname });
+  res.render("index");
+});
 
-    client.query(`set search_path to "groupId4_S10_G3"`,(err,result)=>{
+app.get("/customer", (req, res) => {
+  client.query(`set search_path to "groupId4_S10_G3"`, (err, result) => {
+    if (!err) {
+      console.log("Connected to database");
+    } else {
+      console.log(err);
+    }
+  });
 
-        if(!err)
-        {
-            console.log("Connected to database");
-        }
-        else{
-            console.log(err);
-        }
-        
+  client.query(`Select * from customer`, (err, result) => {
+    if (!err) {
+      res.send(result.rows);
+    } else {
+      console.log(err);
+    }
+  });
 
-    });
-
-    client.query(`Select * from customer`, (err, result) => {
-
-        if (!err) {
-            res.send(result.rows);
-        }
-        else {
-            
-            console.log(err);
-        }
-    })
-
-    client.end;
-})
-
-
-
+  client.end;
+});
